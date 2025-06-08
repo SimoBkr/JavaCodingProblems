47ab4771cd71bbd3bc2ea317f2fd8ff1784df659
SimoBkr <simobkr.sb@gmail.com>
2025-06-08 21:39:43 +0200

    first commit

---
+++ src/main/java/com/simobkr/interviewquestion/FindingNonRepeatedChar.java
@@ -0,0 +1,2 @@
+package com.simobkr.interviewquestion;public class FindingNonRepeatedChar {
+}
---
+++ src/main/java/com/simobkr/interviewquestion/InterviewQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.simobkr.interviewquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewQuestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewQuestionApplication.class, args);
+    }
+
+}
